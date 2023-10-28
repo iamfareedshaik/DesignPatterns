0811ec402cd3f0ca8f1c7834d98dc24a61a1fc27
iamfareedshaik <iamfareedshaik@gmail.com>
2023-10-28 17:45:26 -0500

    updated

--- singleton/MasterSingletonPattern.java
+++ singleton/MasterSingletonPattern.java
@@ -17,6 +17,7 @@ class MasterSingletonPattern{
     public static void main(String args[]){
         SingletonPattern instance = SingletonPattern.getInstance();
         SingletonPattern instance1 = SingletonPattern.getInstance();
+        //SingletonPattern instance = new SingletonPattern() // Constructor is private where object will unable to create
         if(instance == instance1){
             System.out.print("Same instance");
         }
